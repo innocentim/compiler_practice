f0509cdfc6147eb2e2ea37c998789f356d753dfe
Tim Shen <innocentsty@gmail.com>
2012-02-22 13:23:47 +0800

    code_gen.hpp forgotten...

---
+++ code_gen.hpp
@@ -0,0 +1,17 @@
+#ifndef __CODEGEN_H__
+#define __CODEGEN_H__
+
+#include "AST.hpp"
+#include <llvm/Function.h>
+#include <string>
+#include <map>
+
+struct CGContext{
+    CGContext * father;
+    llvm::Function * func;
+    std::map<std::string, llvm::Value*> table;
+    std::map<std::string, Definition*> defTable;
+    CGContext(CGContext * father, llvm::Function * func) : father(father), func(func) {};
+};
+
+#endif
