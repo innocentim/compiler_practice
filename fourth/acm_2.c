d27114efe1b7d5e8249145856280821201449295
Tim Shen <innocentsty@gmail.com>
2012-04-14 21:27:27 +0800

    misc

---
+++ fourth/acm_2.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PRE 0
+#define IN 1
+#define POST 2
+
+struct Tree {
+    char name;
+    struct Tree * l;
+    struct Tree * r;
+};
+
+void create(struct Tree ** root) {
+    char ch;
+    ch = getchar();
+    while (ch == ' ' && ch != EOF) {
+        ch = getchar();
+    }
+    if (ch == EOF) {
+        return;
+    }
+    if (ch == '#') {
+        *root = NULL;
+    } else {
+        *root = malloc(sizeof(struct Tree));
+        (*root)->name = ch;
+        create(&(*root)->l);
+        create(&(*root)->r);
+    }
+};
+
+void trav(struct Tree * root, int method) {
+    if (root == NULL) {
+        return;
+    }
+    if (method == PRE) {
+        printf(" %c", root->name);
+    }
+    trav(root->l, method);
+    if (method == IN) {
+        printf(" %c", root->name);
+    }
+    trav(root->r, method);
+    if (method == POST) {
+        printf(" %c", root->name);
+    }
+};
+
+int node_n = 0;
+int leave_n = 0;
+int one_n = 0;
+int h = 0, hnow = 0;
+
+void stat(struct Tree * root) {
+    if (root == NULL) {
+        return;
+    }
+    if (++hnow > h) {
+        h = hnow;
+    }
+    node_n++;
+    if (!root->l && !root->r) {
+        leave_n++;
+    } else if (!(root->l && root->r)) {
+        one_n++;
+    }
+    stat(root->l);
+    stat(root->r);
+    hnow--;
+};
+
+int main() {
+    struct Tree * root;
+    create(&root);
+    printf("PreOrder:");
+    trav(root, PRE);
+    printf("\n");
+    printf("InOrder:");
+    trav(root, IN);
+    printf("\n");
+    printf("PostOrder:");
+    trav(root, POST);
+    printf("\n");
+    stat(root);
+    printf("%d\n%d %d %d\n", h, node_n, leave_n, one_n);
+
+    return 0;
+};
---
+++ fourth/acm_4.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PRE 0
+#define IN 1
+#define POST 2
+
+struct Tree {
+    char name;
+    struct Tree * l;
+    struct Tree * r;
+};
+
+void create(struct Tree ** root) {
+    char ch;
+    ch = getchar();
+    while (!('A' <= ch && ch <= 'Z' || ch == '#') && ch != '\n') {
+        ch = getchar();
+    }
+    if (ch == '\n') {
+        return;
+    }
+    if (ch == '#') {
+        *root = NULL;
+    } else {
+        *root = malloc(sizeof(struct Tree));
+        (*root)->name = ch;
+        create(&(*root)->l);
+        create(&(*root)->r);
+    }
+};
+
+void pre(struct Tree * root) {
+    if (root == NULL) {
+        return;
+    }
+    printf(" %c", root->name);
+    pre(root->l);
+    pre(root->r);
+};
+
+void mid(struct Tree * root) {
+    struct Tree * queue[1000];
+    int front = 0, rear = 0;
+    if (root) {
+        queue[rear++] = root;
+    }
+    while (front < rear) {
+        struct Tree * temp = queue[front++];
+        printf(" %c", temp->name);
+        if (temp->l) {
+            queue[rear++] = temp->l;
+        }
+        if (temp->r) {
+            queue[rear++] = temp->r;
+        }
+    }
+};
+
+void reverse(struct Tree * root) {
+    if (!root) {
+        return;
+    }
+    struct Tree * temp = root->l;
+    root->l = root->r;
+    root->r = temp;
+    reverse(root->l);
+    reverse(root->r);
+};
+
+int main() {
+    struct Tree * a;
+    struct Tree * b;
+    create(&a);
+    getchar();
+    create(&b);
+    getchar();
+    struct Tree * root = malloc(sizeof(struct Tree));
+    root->l = a;
+    root->r = b;
+    root->name = getchar();
+
+    printf("PreOrder:");
+    pre(root);
+    printf("\n");
+    printf("LevelOrder:");
+    mid(root);
+    printf("\n");
+    reverse(root);
+    printf("PreOrder:");
+    pre(root);
+    printf("\n");
+    printf("LevelOrder:");
+    mid(root);
+    printf("\n");
+
+    return 0;
+};
